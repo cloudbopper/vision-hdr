173e75fa20046874cd7601924ed79e696296e4db
msyamkum <meenakshi.psgit@gmail.com>
2015-02-15 17:28:39 -0600

    Adding BitmapAND function

---
+++ MTB/BitmapAND.m
@@ -0,0 +1,30 @@
+function [ andBm ] = BitmapAND( bm1, bm2 )
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%BitmapAND: AND's the given two bitmaps
+%   Argument: 
+%       bm1 & bm2 - bitmaps of the two images
+%   Return value:
+%       xorBm - AND'ed result between bm1 and bm2
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+rowSize = size(bm1, 1);
+colSize = size(bm1, 2);
+andBm = zeros(rowSize, colSize);
+
+for row = 1:rowSize
+    for col = 1:colSize
+        pixBm1 = bm1(row, col);
+        pixBm2 = bm2(row, col);
+        %AND'ing between the pixels
+        if(pixBm1 == 1 && pixBm2 == 1)
+            andBm(row, col) = 1;
+        else
+            andBm(row, col) = 0;
+        end
+    end
+end
+
+end
+
